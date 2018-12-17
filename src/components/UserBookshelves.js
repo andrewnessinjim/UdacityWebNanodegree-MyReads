67b47c5297e136b903dcc11a5b8a8f242cd94675
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-12-17 11:06:29 +0000

    fix: Remove stray content on screen

--- src/components/UserBookshelves.js
+++ src/components/UserBookshelves.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Bookshelf from './Bookshelf';
 
 const userBookshelves = [{
@@ -29,8 +29,7 @@ function UserBookshelves(props) {
               onNotifyChange={props.onNotifyChange}
             />
           ))}
-          />
-              </div>
+        </div>
       </div>
       <div className="open-search">
         <Link to="/search">Add a book</Link>
